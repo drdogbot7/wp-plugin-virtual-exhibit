df56bc507eab582381b6ce0dc2efa015fe8215d9
Jeremy Mullis <jeremy.mullis@gmail.com>
2025-06-25 14:48:45 -0500

    fix zip packaging, rename online-exhibit to exhibit

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 /**
  * Plugin Name: Virtual Exhibit
  * Description: A WordPress plugin to create and manage virtual exhibits.
- * Version: 1.0.0
- * Author: Your Name
- * Author URI: https://yourwebsite.com
+ * Version: 0.1.0
+ * Author: Jeremy Mullis, Coneflower Consulting
+ * Author URI: https://www.coneflower.org
  * License: GPL2
  */
 
@@ -27,10 +27,10 @@ add_filter('single_template', 'my_custom_template');
 function ddb7_register_cpts()
 {
 	/**
-	 * Post Type Online Exhibit
+	 * Post Type Vircual Exhibit
 	 */
 
-	register_extended_post_type('online-exhibit', [
+	register_extended_post_type('exhibit', [
 		'menu_icon' => 'dashicons-images-alt',
 		'show_in_rest' => true,
 	]);
@@ -60,7 +60,7 @@ function ddb7_add_to_twig($twig)
 
 function ddb7_enqueue_front()
 {
-	$asset_file = include __DIR__ . '/build/index.asset.php';
+	$asset_file = include plugin_dir_path(__FILE__) . '/build/index.asset.php';
 	wp_enqueue_script(
 		'ddb7_js',
 		plugin_dir_url( __FILE__ ) . 'build/index.js',
@@ -75,14 +75,14 @@ function ddb7_enqueue_front()
 	);
 }
 
-function my_custom_template($single) {
+function ddb7_exhibit_template($single) {
 
     global $post;
 
     /* Checks for single template by post type */
     if ( $post->post_type == 'online-exhibit' ) {
-        if ( file_exists( __DIR__ . '/single-online-exhibit.php' ) ) {
-            return __DIR__ . '/single-online-exhibit.php';
+        if ( file_exists( __DIR__ . '/inc/single-online-exhibit.php' ) ) {
+            return __DIR__ . '/inc/single-online-exhibit.php';
         }
     }
 
